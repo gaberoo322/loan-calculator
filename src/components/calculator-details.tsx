2b6c2e6e4a88e454ed9cde117603fd3966215051
Gabe Greenfield <g.greenfield@aspenware.com>
2020-06-09 12:17:56 -0600

    coin collateral logic and styles

--- src/components/calculator-details.tsx
+++ src/components/calculator-details.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 import IconBTC from "cryptocurrency-icons/svg/color/btc.svg"
 import IconLTC from "cryptocurrency-icons/svg/color/ltc.svg"
@@ -50,38 +50,92 @@ const CalculatorDetails: React.FC<FormProps> = (props: FormProps) => {
     }).format(number)}`
   }
 
+  const formatCoinAmount = (coinPrice: number): string => {
+    const formattedCoinCollateral = new Intl.NumberFormat("en").format(
+      totalAmountDue() / coinPrice
+    )
+    return `${formattedCoinCollateral}`
+  }
+
+  const [coinPrices, setCoinPrices] = useState({
+    bitcoin: { usd: 0 },
+    dash: { usd: 0 },
+    dogecoin: { usd: 0 },
+    ethereum: { usd: 0 },
+    litecoin: { usd: 0 },
+  })
+
+  useEffect(() => {
+    const coinIdParams: string = encodeURIComponent(
+      "bitcoin,litecoin,dash,dogecoin,ethereum"
+    )
+    const currencyTypeParams = "usd"
+
+    fetch(
+      `https://api.coingecko.com/api/v3/simple/price?ids=${coinIdParams}&vs_currencies=${currencyTypeParams}`
+    )
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        setCoinPrices(response)
+      })
+      .catch(error => {
+        console.error(error)
+        return {}
+      })
+  }, [props.loanAmount])
+
   return (
     <section className="calculator-details">
-      <div>
-        <div>Monthly Payment ({props.loanTerm} months)</div>
-        <div>{numberToCurrency(monthlyPaymentAmount())}</div>
+      <div className="text-label--wrapper">
+        <div className="label text-label--label ">
+          Monthly Payment ({props.loanTerm} months)
+        </div>
+        <div className="text-label--value">
+          {numberToCurrency(monthlyPaymentAmount())}
+        </div>
       </div>
-      <div>
+      <div className="text-label--wrapper">
         <div>Loan Amount</div>
         <div>{numberToCurrency(props.loanAmount)}</div>
       </div>
-      <div>
+      <div className="text-label--wrapper">
         <div>APR</div>
         <div>{`${(loanInterestAPR() * 100).toFixed(2)}%`}</div>
       </div>
-      <div>
+      <div className="text-label--wrapper">
         <div>Total Loan Cost</div>
         <div>{numberToCurrency(totalAmountDue())}</div>
       </div>
-      <div>
+      <div className="text-label--wrapper">
         <div>Interest</div>
         <div>{numberToCurrency(totalAmountDue() - props.loanAmount)}</div>
       </div>
-      <div>
+      <div className="text-label--wrapper">
         <div>Collateral Needed</div>
         <div>{numberToCurrency(props.loanAmount / props.loanLTV)}</div>
       </div>
-      <div>
-        <img src={IconBTC} alt="Bitcoin Icon" />
-        <img src={IconLTC} alt="Litecoin Icon" />
-        <img src={IconDASH} alt="Dash Icon" />
-        <img src={IconDOGE} alt="Doge Icon" />
-        <img src={IconETH} alt="Ethereum Icon" />
+      <div className="coin--list">
+        <div className="coin--wrapper">
+          <img className="coin--icon" src={IconBTC} alt="Bitcoin Icon" />
+          <span>{`${formatCoinAmount(coinPrices.bitcoin.usd)} BTC`}</span>
+        </div>
+        <div className="coin--wrapper">
+          <img className="coin--icon" src={IconLTC} alt="Litecoin Icon" />
+          <span>{`${formatCoinAmount(coinPrices.litecoin.usd)} LTC`}</span>
+        </div>
+        <div className="coin--wrapper">
+          <img className="coin--icon" src={IconDASH} alt="Dash Icon" />
+          <span>{`${formatCoinAmount(coinPrices.dash.usd)} DASH`}</span>
+        </div>
+        <div className="coin--wrapper">
+          <img className="coin--icon" src={IconDOGE} alt="Doge Icon" />
+          <span>{`${formatCoinAmount(coinPrices.dogecoin.usd)} DOGE`}</span>
+        </div>
+        <div className="coin--wrapper">
+          <img className="coin--icon" src={IconETH} alt="Ethereum Icon" />
+          <span>{`${formatCoinAmount(coinPrices.dogecoin.usd)} DOGE`}</span>
+        </div>
       </div>
     </section>
   )
