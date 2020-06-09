6881fd3764bbf3810af978290fed61c731f6a080
Gabe Greenfield <g.greenfield@aspenware.com>
2020-06-09 05:28:49 -0600

    logic for interest calculations and added icons

--- src/components/calculator-details.tsx
+++ src/components/calculator-details.tsx
@@ -1,5 +1,11 @@
 import React, { useState } from "react"
 
+import IconBTC from "cryptocurrency-icons/svg/color/btc.svg"
+import IconLTC from "cryptocurrency-icons/svg/color/ltc.svg"
+import IconDASH from "cryptocurrency-icons/svg/color/dash.svg"
+import IconDOGE from "cryptocurrency-icons/svg/color/doge.svg"
+import IconETH from "cryptocurrency-icons/svg/color/eth.svg"
+
 interface FormProps {
   loanAmount: number
   loanTerm: number
@@ -8,54 +14,75 @@ interface FormProps {
 }
 
 const CalculatorDetails: React.FC<FormProps> = (props: FormProps) => {
-  console.log("LOAN-DETAILS-RENDER")
   const loanInterestAPR = (): number => {
     const deviation = props.loanLTV - 6
     const APR = (10 + deviation) / 100
     return APR
   }
-  const loanInterestAmount = (): number => {
-    const interestAmount =
-      5000 * (1 + loanInterestAPR() / 12) ** props.loanTerm - 5000
-    console.log(interestAmount)
-    return interestAmount
-  }
 
   const monthlyPaymentAmount = (): number => {
+    const P = props.loanAmount
+    const r = loanInterestAPR() / 12
+    const n = props.loanTerm
     if (props.loanRepaymentOption === "IO") {
-      return loanInterestAmount() / props.loanTerm
+      return P * r
     } else if (props.loanRepaymentOption === "PI") {
-      return (loanInterestAmount() + props.loanAmount) / props.loanTerm
+      return (P * (r * (1 + r) ** n)) / ((1 + r) ** n - 1)
+    } else {
+      console.error("invalid repayment option")
     }
   }
 
+  const totalAmountDue = (): number => {
+    if (props.loanRepaymentOption === "IO") {
+      return monthlyPaymentAmount() * props.loanTerm + props.loanAmount
+    } else if (props.loanRepaymentOption === "PI") {
+      return monthlyPaymentAmount() * props.loanTerm
+    } else {
+      console.error("invalid repayment option")
+    }
+  }
+
+  const numberToCurrency = (number: number): string => {
+    return `$${new Intl.NumberFormat("en", {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(number)}`
+  }
+
   return (
-    <section>
+    <section className="calculator-details">
       <div>
-        <div>Monthly Payments</div>
-        <div>{monthlyPaymentAmount()}</div>
+        <div>Monthly Payment ({props.loanTerm} months)</div>
+        <div>{numberToCurrency(monthlyPaymentAmount())}</div>
       </div>
       <div>
         <div>Loan Amount</div>
-        <div>{props.loanAmount}</div>
+        <div>{numberToCurrency(props.loanAmount)}</div>
       </div>
       <div>
         <div>APR</div>
-        <div>{loanInterestAPR()}</div>
+        <div>{`${(loanInterestAPR() * 100).toFixed(2)}%`}</div>
       </div>
       <div>
         <div>Total Loan Cost</div>
-        <div>{loanInterestAmount() + props.loanAmount}</div>
+        <div>{numberToCurrency(totalAmountDue())}</div>
       </div>
       <div>
         <div>Interest</div>
-        <div>{loanInterestAmount()}</div>
+        <div>{numberToCurrency(totalAmountDue() - props.loanAmount)}</div>
       </div>
       <div>
         <div>Collateral Needed</div>
-        <div>asdfasdf</div>
+        <div>{numberToCurrency(props.loanAmount / props.loanLTV)}</div>
       </div>
-      <div>coin icons will go here :)</div>
+      <div>
+        <img src={IconBTC} alt="Bitcoin Icon" />
+        <img src={IconLTC} alt="Litecoin Icon" />
+        <img src={IconDASH} alt="Dash Icon" />
+        <img src={IconDOGE} alt="Doge Icon" />
+        <img src={IconETH} alt="Ethereum Icon" />
+      </div>
     </section>
   )
 }
